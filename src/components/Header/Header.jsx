9d15f09bbd062bc470fc00ece63e150245ca4a2b
Melina Zarate <melinazarate@Melinas-MacBook-Pro.local>
2024-07-04 17:11:58 -0300

    Add layout component Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
+import styles from './Header.module.css';
 
 const Header = () => {
     return(
         <>
+       <section className={styles.header}>
+        <div className={styles.headerContent}>
+            <h1 className={styles.title}>Hola, mi nombre es <span>Sol</span></h1>
+            <h3 className={styles.textAnimation}>Soy 
+                <span>Desarrolladora Front-End</span></h3>
+                
+        </div>
+       </section>
         </>
     )
 
