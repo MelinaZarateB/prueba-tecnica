477dfb10d10c775dd6d46ef6619a3d3640e7abfb
Melina Zarate <melinazarate@Melinas-MacBook-Pro.local>
2024-07-04 21:36:08 -0300

    Finish Header Component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,13 @@
-
+import styles from './About.module.css';
 
 const About = () => {
     return(
         <>
-       
+        <section className={styles.aboutContainer}>
+           <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur id ratione autem maxime! Omnis minima fugit labore dolore enim sapiente delectus, error tempore nemo sit eos est velit veniam ex.
+           </p>
+        </section>
         </>
     )
 
