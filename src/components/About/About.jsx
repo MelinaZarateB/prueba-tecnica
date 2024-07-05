1a109dedac99242f0ebb316f3a76dba087eda8fe
Melina Zarate <melinazarate@Melinas-MacBook-Pro.local>
2024-07-05 01:35:22 -0300

    Add Interests Component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,5 @@
 import styles from './About.module.css';
+import Interests from '../Interests/Interests';
 
 const About = () => {
     return(
@@ -10,7 +11,10 @@ const About = () => {
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus tempore doloremque nemo soluta repellendus cupiditate assumenda molestias, eius fugit commodi ipsum, et iure esse impedit. Fugiat incidunt iste et perferendis.</p>
             </div>
          </div>
-         <div></div>
+         <div>
+            <Interests></Interests>
+
+         </div>
         </section>
         </>
     )
--- src/components/Interests/Interests.jsx
+++ src/components/Interests/Interests.jsx
@@ -1,7 +1,57 @@
+'use client'
+
+import styles from './Interests.module.css';
+import { useRef } from 'react';
+import useNearScreen from '../hooks/useNearScreen';
 
 const Interests = () => {
+
+    const elementRef = useRef();
+    const show = useNearScreen(elementRef)
+
     return(
         <>
+         <section className={styles.interests}>
+        <div className={styles.sectionContent}>
+            <h4 className={styles.title}>Intereses</h4>
+            <div className={styles.row}>
+                <div className={styles.col}>
+                <div className={styles.skill}>
+                        <span className={styles.span}>Desarrollo Web</span>
+                        <div className={styles.interestBar}>
+                            <div className={`${styles.progress} ${show ? styles.webDevelopment : ''}`} ref={elementRef}>
+                                <span className={styles.spanBarraSkill}>99%</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.skill}>
+                        <span className={styles.span}>Diseño UX UI</span>
+                        <div className={styles.interestBar}>
+                            <div className={`${styles.progress} ${show ? styles.design : ''}`} ref={elementRef}>
+                                <span className={styles.spanBarraSkill}>80%</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.skill}>
+                        <span className={styles.span}>Deportes</span>
+                        <div className={styles.interestBar}>
+                            <div className={`${styles.progress} ${show ? styles.sports : ''}`} ref={elementRef}>
+                                <span className={styles.spanBarraSkill}>75%</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.skill}>
+                        <span className={styles.span}>Lectura</span>
+                        <div className={styles.interestBar}>
+                            <div className={`${styles.progress} ${show ? styles.reading : ''}`} ref={elementRef}>
+                                <span className={styles.spanBarraSkill}>73%</span>
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </section>
         </>
     )
 
